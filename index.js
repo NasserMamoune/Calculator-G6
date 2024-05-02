b641f2eec59d7d02422fe4102a0d5e0ba9197ed9
Nasser Mamoune <medsaleckmamoune@gmail.com>
2024-05-02 12:49:53 +0000

    fixed errors

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 function calculate(operation) {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
-
+    
     let result;
 
     switch (operation) {
         case 'add':
+            var num3 = parseFloat(document.getElementById("num3").value);
+            if (!isNaN(num3)){
+                result = num1 + num2 + num3;
+            } 
+            else {
                 result = num1 + num2;
-            break;
-        case
-            'add' :
-            var num3 = parseFloat(document.getElementById ("num3"). value);
-            if (!isNaN(num3)) {
-            result = num1 + num2 + num3;
-             } else {
-            result = num1 + num2;
-             }
+            }
             break;    
         case 'subtract':
             result = num1 - num2;
