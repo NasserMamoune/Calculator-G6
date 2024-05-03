8d77515c87fcfffa7ce51098ab1bdbe631e8f8f9
Nasser Mamoune <medsaleckmamoune@gmail.com>
2024-05-03 03:17:01 +0000

    fixed addition bug

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ function calculate(operation) {
 
     switch (operation) {
         case 'add':
+            var num3 = parseFloat(document.getElementById("num3").value);
+            if (!isNaN(num3)) {
+                result = num1 + num2 + num3;
+            } else {
                 result = num1 + num2;
+            }
             break;
         case 'subtract':
             result = num1 - num2;
             break;
         case 'multiply':
+            var num3 = parseFloat(document.getElementById("num3").value);
+            if (!isNaN(num3)) {
+                result = num1 * num2 * num3;
+            } else {
                 result = num1 * num2;
+            }
             break;
         case 'divide':
             if (num2 === 0) {
